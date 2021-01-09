68d58076386bd2f2567a14967fa886f694181801
程家山 <chengjs@tuya.com>
2021-01-09 18:37:40 +0800

    更新libcserialport.dll库，修复CTS和DSR信号错误判断bug

--- CSerialPort.DotNet/Program.cs
+++ CSerialPort.DotNet/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.IO;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSerialPort.DotNet
 {
@@ -30,14 +31,15 @@ namespace CSerialPort.DotNet
             //manager.Close();
             //Console.WriteLine(manager.IsOpen().ToString());
 
-            var port = new System.IO.Ports.SerialPort();
+            var data = new byte[1024];
 
-            port.PortName = "COM12";
+            var port = new SerialPort();
+            port.PortName = "COM44";
             port.BaudRate = 9600;
             port.Open();
 
             //Console.WriteLine(port.IsOpen.ToString());
-            //var data = new byte[1024];
+
             //var a = port.DtrEnable;
             //for (int i = 0; i < 200; i++)
             //{
@@ -50,20 +52,30 @@ namespace CSerialPort.DotNet
             //}
             //port.BaudRate = 115200;
             //Thread.Sleep(5000);
-            //for (int i = 0; i < 200; i++)
+            //var task = new Task(() =>
             //{
-            //    //var readCount = port.Read(data, 0, 1024);
-            //    //if (readCount > 0) Console.WriteLine(System.Text.Encoding.UTF8.GetString(data, 0, readCount));
-            //    //port.RtsEnable = !port.RtsEnable;
-            //    //Console.WriteLine(port.RtsEnable);
-            //    port.Write(new byte[] { 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01 }, 0, 4);
-            //    Thread.Sleep(1);
-            //}
+            //    while (true)
+            //    {
+            //        var readCount = port.Read(data, 0, 1024);
+            //        if (readCount > 0) Console.WriteLine(System.Text.Encoding.UTF8.GetString(data, 0, readCount));
+            //        Thread.Sleep(1);
+            //    }
+            //});
+            //task.Start();
+            for (int i = 0; i < 200000; i++)
+            {
+                //var readCount = port.Read(data, 0, 1024);
+                //if (readCount > 0) Console.WriteLine(System.Text.Encoding.UTF8.GetString(data, 0, readCount));
+                //port.RtsEnable = !port.RtsEnable;
+                Console.WriteLine(port.CtsHolding);
+                //port.Write(new byte[] { 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01 }, 0, 4);
+                Thread.Sleep(1);
+            }
+            ////Console.ReadKey();
+            //port.Write(new byte[] { 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01 }, 0, 26);
             //Console.ReadKey();
-            port.Write(new byte[] { 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0x55, 0xff, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01, 0xaa, 0x01 }, 0, 26);
-            Console.ReadKey();
-            port.Close();
-            Console.WriteLine(port.IsOpen.ToString());
+            //port.Close();
+            //Console.WriteLine(port.IsOpen.ToString());
         }
     }
 }
